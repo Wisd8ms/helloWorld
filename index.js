e4e0998fe24ca871b53296bd45632f0d83c9bc9f
wisd8ms <kjoshua473@gmaail.com>
2023-09-25 13:04:09 -0500

    bye world

---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict'
+console.log("hello world")
+
+let x = 50+89999
+console.log("x = " + x)
+
+let y = 40-0.9999999999
+console.log(y)
+let z = 87*98
+console.log(z)
+let a = 86/8675
+console.log(a)
+let b = 2
+
+// comment 
+console.log(b**3)
+
+if(23%5==2){
+    console.log(true)
+}
+else{
+    console.log(false)
+    console.log(23%5)
+}
