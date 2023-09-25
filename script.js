528fa3c82e404756b11b3f2658733fe3728ee56d
wisd8ms <kjoshua473@gmaail.com>
2023-09-25 13:37:40 -0500

    end for today

---
+++ script.js
@@ -0,0 +1,59 @@
+//strings
+// The quick brown fox jumped over the log.
+// Peter gave Susan 2 oranges.
+// Step Into the Maze by Angela Engel
+// Deion said that he would be great.
+// Explore the Horizon. 
+
+// 34 + 21
+// 22 - 12
+// 19-32
+// 21+45
+// 2*89
+// 5*43
+// 12/12
+// 25/5
+
+let myVar = 'some string'
+console.log(myVar)
+
+let sliper = "The quick brown fox jumped over the log."
+console.log(sliper)
+
+let floor = "Peter gave Susan 2 oranges."
+
+let roof = "Step Into the Maze by Angela Engel"
+console.log(roof)
+
+let  bed = "Deion said that he would be great."
+console.log(bed)
+
+let  table = "Explore the Horizon. "
+console.log(table)
+
+let wonder = 34 + 21
+console.log(wonder)
+
+let ihave = 
+22 - 12
+console.log(ihave)
+
+let noidea= 19-32
+console.log(noidea)
+
+let pass = 21+45
+console.log(pass)
+
+let serial= 2*89
+console.log(serial)
+
+let number = 5*43
+console.log(number)
+
+let house = 12/12
+console.log(house)
+
+let mouse = 25/5
+console.log(mouse)
+
+
